6af8a8513c40bb129efef3e496957441d1f1f075
Duy Phan Manh <phanmanhduy873333@gmail.com>
2022-07-26 22:29:57 +0700

    btvnbuoi3

--- Buoi_3/src/Bai_2.java
+++ Buoi_3/src/Bai_2.java
@@ -23,40 +23,18 @@ public class Bai_2 {
         String s1[] = s.split(" ");
         s = "";
         for (int i = 0; i < s1.length; i++) {
-//            s += String.valueOf(s1[i].charAt(0)).toUpperCase() + s1[i].substring(1);
-            s+=s1[i].substring(0,1).toUpperCase()+s1[i].substring(1);
+
+            s += s1[i].substring(0, 1).toUpperCase() + s1[i].substring(1);
             if (i < s1.length - 1) {
                 s += " ";
             }
         }
         return s;
     }
-    public static String xoaSo(String s)
-    {
-        return s.replaceAll("[0-9]","");
+
+    public static String xoaSo(String s) {
+        return s.replaceAll("[0-9]", "");
     }
-//    public static boolean kiemTraSo(char a) {
-//        if ((int) a >= 48 && (int) a <= 57)
-//            return true;
-//        else
-//            return false;
-//    }
-//
-//    public static String xoaKyTu(String s, int a) {
-//        return s.substring(0, a) + s.substring(a + 1);
-//    }
-//
-//    public static String xoaSo(String s) {
-//
-//        int n = s.length();
-//        for (int i = 0; i < n; i++) {
-//            if (kiemTraSo(s.charAt(i)) == true) {
-//                s = xoaKyTu(s, i);
-//                i--;
-//                n--;
-//            }
-//        }
-//        return s;
-//    }
+
 }
 
